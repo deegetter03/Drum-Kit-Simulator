634b7ba1d288cb8b927b406ea5e9b666adddfd31
DEEPAK KUMAR <116636205+deegetter03@users.noreply.github.com>
2023-08-27 20:07:47 +0530

    JS Event Listener

---
+++ script.js
@@ -0,0 +1,65 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0 ;i<numberOfDrumButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        
+        var buttonchange = this.innerHTML;
+        makesound(buttonchange);
+        buttonAnimation(buttonchange)
+    });
+}
+
+document.addEventListener("keypress", function(event){
+    makesound(event.key);
+    buttonAnimation(event.key);
+});
+
+function makesound(key) {
+    switch(key){
+        case "d":
+            var tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+
+         case "r":
+            var tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+
+         case "u":
+            var tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+            
+        case "m":
+            var tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+                
+        case "k":
+            var snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+
+        case "i":
+            var crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+
+        case "t":
+            var kick = new Audio('sounds/kick.mp3');
+            kick.play();
+            break;
+
+        default : console.log(key);    
+    }
+}
+
+function buttonAnimation(currentkey) {
+    var activeButton = document.querySelector("." + currentkey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed");
+        }, 100);
+}
